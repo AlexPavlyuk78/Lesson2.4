f60180869f57043053bced3985933f369676e668
Alex78ol <119539530+Alex78ol@users.noreply.github.com>
2023-01-06 16:01:06 +0100

    Set up Main Label

--- Lesson2.4/ViewController.swift
+++ Lesson2.4/ViewController.swift
@@ -9,12 +9,31 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // MARK: - IB Outlets.
+    
     @IBOutlet var segmentedControl: UISegmentedControl!
+    @IBOutlet var mainLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Here we working on Interface action with segmentedControl.
+        
+        segmentedControl.insertSegment(withTitle: "Third", at: 2, animated: false)
+        setupMainLabel()
+        
     }
-
+    // MARK: - IB Actions.
+    
+    @IBAction func segmentedControlAction() {
+    }
+    
+    // MARK: - Private method.
+    private func setupMainLabel() {
+        // we have to move here all logical parameters.
+        mainLabel.text = "Ну Погоди!!!"
+        mainLabel.font = UIFont.systemFont(ofSize: 35)
+        mainLabel.textAlignment = .center
+        mainLabel.numberOfLines = 2
+        
+    }
 }
-
